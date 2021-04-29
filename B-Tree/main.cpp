79043e20d3fd3bb6e834e2e5069f243508b8ca7e
Vali Bojici <valibojici@gmail.com>
2021-04-29 17:51:23 +0300

    test update

--- B-Tree/main.cpp
+++ B-Tree/main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <random>
+#include <vector>
+#include <time.h>
 #include "BTree.h"
+
+template<class T>
+void print(const std::vector<T>& vals,const char* sep = " ", const char* end = "\n") {
+    if (vals.empty())return;
+    std::cout << vals[0];
+    for (int i = 1; i < vals.size(); ++i)
+        std::cout << sep << vals[i];
+    std::cout << end;
+}
+
 int main()
 {
     // TO DO m_splitChild 
+    std::vector<int> v;
+    srand(time(0));
+    for (int i = 0; i < 1000; ++i)
+        v.push_back(rand() % 3000);
+    
 
-    BTree<int> b(3);
-    b.Insert(1);
-    b.Insert(2);
-    b.Insert(3);
-    b.Insert(4);
-    b.Insert(5);
-    b.Insert(5);
-    /*b.Insert(6);
-    b.Insert(7);
-    b.Insert(8);
-    b.Insert(9);
-    b.Insert(10);*/
-    b.InOrdine(std::cout);
+    /*print(v);*/
+    
+    BTree<int> b(10);
+
+    for (int i : v)
+        b.Insert(i);
+    
+    std::sort(v.begin(), v.end());
+ 
+    auto vals = b.InOrdine();
+    /* print(vals); */
+    
+    std::cout << ((vals == v) ? "True" : "False") << ' ' << b.m_height;
 }
